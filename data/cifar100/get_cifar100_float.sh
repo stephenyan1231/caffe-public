d5979cfac110c776c609712b730671f431c9eaf3
Zhicheng Yan <zyan3@illinois.edu>
2014-09-10 15:00:35 -0500

    add get_cifar100_float.sh

---
+++ data/cifar100/get_cifar100_float.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+# This scripts downloads the CIFAR10 (binary version) data and unzips it.
+
+DIR="$( cd "$(dirname "$0")" ; pwd -P )"
+cd $DIR
+
+echo "Downloading..."
+
+#for i in `seq 1 5`
+#do
+#	file=`printf "https://dl.dropboxusercontent.com/u/44884434/cifar100/float_data_batch_%d.bin" $i`
+#	wget --no-check-certificate ${file} 
+#done
+
+wget --no-check-certificate https://dl.dropboxusercontent.com/u/44884434/cifar100/float_test_batch.bin
+
+echo "Done."
