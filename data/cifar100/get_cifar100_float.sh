4bc24f47d92fb4fadebc27e2701b4a45445d5336
Zhicheng Yan <zyan3@illinois.edu>
2014-09-11 16:29:57 -0500

    update get_cifar100_float.sh

--- data/cifar100/get_cifar100_float.sh
+++ data/cifar100/get_cifar100_float.sh
@@ -6,11 +6,11 @@ cd $DIR
 
 echo "Downloading..."
 
-#for i in `seq 1 5`
-#do
-#	file=`printf "https://dl.dropboxusercontent.com/u/44884434/cifar100/float_data_batch_%d.bin" $i`
-#	wget --no-check-certificate ${file} 
-#done
+for i in `seq 1 5`
+do
+	file=`printf "https://dl.dropboxusercontent.com/u/44884434/cifar100/float_data_batch_%d.bin" $i`
+	wget --no-check-certificate ${file} 
+done
 
 wget --no-check-certificate https://dl.dropboxusercontent.com/u/44884434/cifar100/float_test_batch.bin
 
