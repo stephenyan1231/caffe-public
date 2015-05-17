2f873941b8ee25f0643fd2f4843ba1be461c4cdb
Zhicheng Yan <zyan3@zyan3-2.cs.illinois.edu>
2015-05-17 15:25:13 -0500

    update video-enhance/prepare_training_data_uniform_set_xpro.sh

--- video-enhance/prepare_training_data_uniform_set_xpro.sh
+++ video-enhance/prepare_training_data_uniform_set_xpro.sh
@@ -15,6 +15,6 @@ GLOG_logtostderr=1 $VIDEO_ENHANCE_BINS/train.bin  \
 --global_feature_lmdb_path=video-enhance/data/uniform_set/global_ftr_lmdb \
 --semantic_context_feature_binary_dirs=/home/zyan3/local/proj/caffe_private_hdcnn/examples/uniform_set/VGG_ILSVRC_16_layers_parser_fc7/stitch_fc7_longside_1024/,\
 /home/zyan3/local/proj/caffe_private_hdcnn/examples/uniform_set/VGG_ILSVRC_16_layers_parser_fc7/stitch_fc7_longside_2048/ \
---out_training_segment_lmdb=video-enhance/examples/uniform_set/image-enhance-train-lmdb 
+--out_training_segment_lmdb=${OUT_TRAINING_SEGMENT_LDMB} 
 
 echo "Done."
