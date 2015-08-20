46b321986a41ed47cd7f951901619fdc93461c05
Zhicheng Yan <zyan3@zyan3-2.cs.illinois.edu>
2015-08-19 23:13:54 -0500

    supplementary commit IV :  clean code to prepare for code release. CIFAR100 dataset

---
+++ examples/cifar100/get_cifar100_float_train-train-val-leveldb.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# This scripts downloads the CIFAR10 (binary version) data and unzips it.
+
+DIR="$( cd "$(dirname "$0")" ; pwd -P )"
+cd $DIR
+
+echo "Downloading..."
+
+
+wget --no-check-certificate https://dl.dropboxusercontent.com/u/44884434/cifar100/cifar100-float-train-train-val-leveldb.tar.gz 
+
+tar -xzvf cifar100-float-train-train-val-leveldb.tar.gz
+
+echo "Done."
